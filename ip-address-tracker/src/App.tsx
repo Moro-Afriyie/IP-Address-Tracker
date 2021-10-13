cc0d1d1b184f3925bbb9152d8a289056f5c01ce5
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-13 11:38:29 +0000

    created a custom hook for api calls

--- ip-address-tracker/src/App.tsx
+++ ip-address-tracker/src/App.tsx
@@ -5,9 +5,9 @@ import Search from "./components/Search";
 import Map from "./components/Map";
 import { useFetch } from "./customHooks/UseFetch";
 
-const url = "";
+// const url = "";
 function App() {
-  const response: string[] | null = useFetch(url);
+  // const response: string[] | null = useFetch(url);
   return (
     <main className="App">
       <Search />
