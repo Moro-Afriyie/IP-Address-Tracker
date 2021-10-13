c2729ef92356fe7a1a66bfb39d2456963d51904c
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-13 11:17:52 +0000

    added the various components

---
+++ ip-address-tracker/src/components/Display.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IDisplayProps {}
+
+const Display: React.FunctionComponent<IDisplayProps> = (props) => {
+  return;
+};
+
+export default Display;
---
+++ ip-address-tracker/src/components/Search.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ISearchProps {}
+
+const Search: React.FunctionComponent<ISearchProps> = (props) => {
+  return;
+};
+
+export default Search;
