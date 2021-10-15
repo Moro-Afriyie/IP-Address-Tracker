2f3d297a7eef966df2532aa8bee9ab4b0f01f974
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-15 16:51:30 +0000

    styled the display component

--- ip-address-tracker/src/components/Display.tsx
+++ ip-address-tracker/src/components/Display.tsx
@@ -7,7 +7,32 @@ interface IDisplayProps {}
 const Display: React.FunctionComponent<IDisplayProps> = (props) => {
   return (
     <div className="display">
-      <h1>Display</h1>
+      <div className="display__section">
+        <h6 className="title">ip address</h6>
+        <div className="info">
+          <h2>192.212.174.101</h2>
+        </div>
+      </div>
+      <div className="display__section">
+        <h6 className="title">location</h6>
+        <div className="info">
+          <h2>Booklyn, NY</h2>
+          <h2>10001</h2>
+        </div>
+      </div>
+      <div className="display__section">
+        <h6 className="title">time zone</h6>
+        <div className="info">
+          <h2>UTC-05:00</h2>
+        </div>
+      </div>
+      <div className="display__section">
+        <h6 className="title">isp</h6>
+        <div className="info">
+          <h2>SpaceX</h2>
+          <h2>Starlink</h2>
+        </div>
+      </div>
     </div>
   );
 };
