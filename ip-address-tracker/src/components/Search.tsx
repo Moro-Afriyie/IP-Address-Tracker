e83cafc18e8ae8bfffe760a616a9103dfe88fa77
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-15 16:01:43 +0000

    styled the search component

--- ip-address-tracker/src/components/Search.tsx
+++ ip-address-tracker/src/components/Search.tsx
@@ -1,15 +1,22 @@
 import * as React from "react";
 import "../styles/search.scss";
 import arrow from "../images/icon-arrow.svg";
+import pattern from "../images/pattern-bg.png";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ISearchProps {}
 
 const Search: React.FunctionComponent<ISearchProps> = (props) => {
   return (
-    <div className="search">
+    <div className="search" style={{ backgroundImage: `url(${pattern})` }}>
       <div className="search__input">
-        <input type="text" name="ipAddress" id="ipAddress" value="" />
+        <input
+          type="text"
+          name="ipAddress"
+          id="ipAddress"
+          value=""
+          placeholder="Search for any IP address or domain "
+        />
         <button>
           <img src={arrow} alt="arrow icon" />
         </button>
