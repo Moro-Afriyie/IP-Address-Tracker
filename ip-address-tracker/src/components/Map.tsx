45d8934a61aa4d234b7113b532db94cbd495d8ef
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-15 15:01:09 +0000

    added the various templates for the various pages

--- ip-address-tracker/src/components/Map.tsx
+++ ip-address-tracker/src/components/Map.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
-import "../styles/search.map.scss";
+import "../styles/map.scss";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IMapProps {}
 
 const Map: React.FunctionComponent<IMapProps> = (props) => {
-  return <h1>Map</h1>;
+  return (
+    <div className="map">
+      <h1>Map</h1>
+    </div>
+  );
 };
 
 export default Map;
--- ip-address-tracker/src/components/Search.tsx
+++ ip-address-tracker/src/components/Search.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import "../styles/search.scss";
+import arrow from "../images/icon-arrow.svg";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ISearchProps {}
 
 const Search: React.FunctionComponent<ISearchProps> = (props) => {
-  return <h1>Search</h1>;
+  return (
+    <div className="search">
+      <div className="search__input">
+        <input type="text" name="ipAddress" id="ipAddress" value="" />
+        <button>
+          <img src={arrow} alt="arrow icon" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
