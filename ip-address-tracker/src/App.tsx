111066fdcad791bb9d68df8f57908df0faa9af12
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-16 17:12:15 +0000

    added the leaflet link to the header

--- ip-address-tracker/src/App.tsx
+++ ip-address-tracker/src/App.tsx
@@ -6,24 +6,24 @@ import Map from "./components/Map";
 import { useFetch } from "./customHooks/UseFetch";
 import { API_KEY } from "./customHooks/apiKey";
 
-interface IPResponse {
-  location: {
-    country: string;
-    region: string;
-    timezone: string;
-  };
-  ip: string;
-  isp: string;
-}
-const url = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}`;
+// interface IPResponse {
+//   location: {
+//     country: string;
+//     region: string;
+//     timezone: string;
+//   };
+//   ip: string;
+//   isp: string;
+// }
+// const url = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}`;
 function App() {
-  const response: IPResponse | null = useFetch(url);
-  const location = response?.location;
-  const ip = response?.ip;
-  const isp = response?.isp;
-  console.log({ location });
-  console.log(ip);
-  console.log(isp);
+  // const response: IPResponse | null = useFetch(url);
+  // const location = response?.location;
+  // const ip = response?.ip;
+  // const isp = response?.isp;
+  // console.log({ location });
+  // console.log(ip);
+  // console.log(isp);
   // console.log(response);
   return (
     <main className="container">
