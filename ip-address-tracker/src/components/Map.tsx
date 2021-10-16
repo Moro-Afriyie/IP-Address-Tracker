1b0b8f109824ed8646dc7ac695cd329268495e54
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-16 17:58:39 +0000

    installed the react-leaflet package

--- ip-address-tracker/src/components/Map.tsx
+++ ip-address-tracker/src/components/Map.tsx
@@ -1,14 +1,25 @@
 import * as React from "react";
 import "../styles/map.scss";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IMapProps {}
 
 const Map: React.FunctionComponent<IMapProps> = (props) => {
+  const position: number[] = [51.505, -0.09];
+
   return (
-    <div className="map">
-      <h1>Map</h1>
-    </div>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
---
+++ ip-address-tracker/src/customHooks/apiKey.tsx
@@ -0,0 +1,1 @@
+export const API_KEY = "at_pCdM6RgqExKOLZ2ja8hvLNJgGsCCB";
