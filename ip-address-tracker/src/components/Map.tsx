97cf2c808bcebf79e268a84e209e89dec37f99e5
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-16 18:40:41 +0000

    fixed the react-leaflet bug

--- ip-address-tracker/src/components/Map.tsx
+++ ip-address-tracker/src/components/Map.tsx
@@ -6,20 +6,22 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 interface IMapProps {}
 
 const Map: React.FunctionComponent<IMapProps> = (props) => {
-  const position: number[] = [51.505, -0.09];
+  // const position:number[] = [51.505, -0.09];
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
