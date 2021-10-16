94e3f9ccd05ec67c3472f9d63b3a298cf700072e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-16 19:06:11 +0000

    added the leaflet map to the project

--- ip-address-tracker/src/components/Map.tsx
+++ ip-address-tracker/src/components/Map.tsx
@@ -10,7 +10,7 @@ const Map: React.FunctionComponent<IMapProps> = (props) => {
 
   return (
     <div className="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
