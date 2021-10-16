860b55691bf65ba4da60d9312908c6bbb3aaa54b
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-16 19:26:17 +0000

    fixed the leaflet bug

--- ip-address-tracker/src/components/Map.tsx
+++ ip-address-tracker/src/components/Map.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import "../styles/map.scss";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import marker from "../images/icon-location.svg";
+// import Marker from "react-leaflet-enhanced-marker";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IMapProps {}
@@ -15,7 +17,10 @@ const Map: React.FunctionComponent<IMapProps> = (props) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker
+          // icon={<img src={marker} style={{ width: "100" }} />}
+          position={[51.505, -0.09]}
+        >
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
