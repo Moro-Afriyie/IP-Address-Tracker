aa45829a51e46a4b56cd161e0cb43a16c4cede37
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-18 08:31:00 +0000

    fixed the lattitude and longitude bug

--- ip-address-tracker/src/components/Map.tsx
+++ ip-address-tracker/src/components/Map.tsx
@@ -3,28 +3,40 @@ import "../styles/map.scss";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import marker from "../images/icon-location.svg";
 import { IpAddressContext } from "../contexts/IpAddressContext";
+import { useState, useEffect } from "react";
+import { API_KEY } from "../customHooks/apiKey";
 // import Marker from "react-leaflet-enhanced-marker";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IMapProps {}
 
 const Map: React.FunctionComponent<IMapProps> = (props) => {
-  // const position:number[] = [51.505, -0.09];
   const result = React.useContext(IpAddressContext);
-  // const lat = result.response.location.lat;
-  // const long = result.response.location.lng;
-  // console.log(lat, long);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(-1);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${result?.response?.ip}`
+      );
+      const data = await res.json();
+      setLat(data.location.lat);
+      setLng(data.location.lng);
+    };
+
+    fetchData();
+  }, [result?.response?.ip]);
 
   return (
     <div className="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker
           // icon={<img src={marker} style={{ width: "100" }} />}
-          position={[51.505, -0.09]}
+          position={[lat, lng]}
         ></Marker>
       </MapContainer>
     </div>
